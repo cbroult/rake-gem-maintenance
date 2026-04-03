4264560f3834a66e7e65433ff2ad8cb3c86cf7de
Christophe Broult <cbroult@yahoo.com>
2026-04-03 12:58:13 +0200

    chore: remove deprecated `rake/gem_maintenance/install_tasks` file

--- lib/rake/gem_maintenance/install_tasks.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-# TODO: Remove after releasing version that drops support for require "rake/gem_maintenance/install_tasks"
-# This file exists for backward compatibility. Consumers should migrate to:
-# require "rake/gem/maintenance/install_tasks"
-warn 'DEPRECATED: require "rake/gem_maintenance/install_tasks" is deprecated. ' \
-     'Use require "rake/gem/maintenance/install_tasks" instead.'
-require "rake/gem/maintenance/install_tasks"
